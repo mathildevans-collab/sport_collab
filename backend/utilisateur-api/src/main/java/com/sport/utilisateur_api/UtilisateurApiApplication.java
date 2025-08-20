364730efc732377296a118ef9c789602de80b134
LeMatt-ux <matthieubtnt@gmail.com>
2025-08-20 09:11:39 +0200

    Supprime les fichiers/dossiers devenus inutiles

---
+++ backend/utilisateur-api/src/main/java/com/sport/utilisateur_api/UtilisateurApiApplication.java
@@ -0,0 +1,13 @@
+package com.sport.utilisateur_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisateurApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilisateurApiApplication.class, args);
+	}
+
+}
